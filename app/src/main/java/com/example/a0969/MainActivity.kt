175337b9a013c41ce22bdf07f0b0d104eb372d68
Isnawan25 <isnawanrifai@gmail.com>
2024-03-13 14:44:55 +0700

    initial commit

---
+++ app/src/main/java/com/example/a0969/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.a0969
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a0969.ui.theme._0969Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _0969Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Isnawan Rifai Saputra 23.02.0969")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+     Surface(color = Color.Green) {
+        Text(
+            text = "Hello $name! How are you?",
+            style = TextStyle(fontSize = 35.sp,
+                textAlign = TextAlign.Center),
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _0969Theme {
+        Greeting("Isnawan Rifai Saputra 23.02.0969")
+    }
+}
